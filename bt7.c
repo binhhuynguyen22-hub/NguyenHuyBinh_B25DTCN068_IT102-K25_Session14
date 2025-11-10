99faf956209064de8f25620066779adbe1f2894c
binhhuynguyen22-hub <binhhuynguyen22@gmail.com>
2025-11-10 21:51:39 +0700

    Update bt7.c

--- bt7.c
+++ bt7.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-int laSoNguyenTo(int n) {
-    if (n < 2) return 0;
-    for (int i = 2; i <= sqrt(n); i++)
-        if (n % i == 0)
-            return 0;
-    return 1;
-}
+int laSoNguyenTo(int n);
 
 int main() {
     int x;
     printf("Nhap mot so: ");
     scanf("%d", &x);
 
-    if (laSoNguyenTo(x))
-        printf("%d la so nguyen to.\n", x);
+    if (laSoNguyenTo(x)==1)
+        printf("true");
     else
-        printf("%d khong phai la so nguyen to.\n", x);
+        printf("false");
 
     return 0;
+}
+int laSoNguyenTo(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i <= sqrt(n); i++){
+        if (n % i == 0)
+            return 0;}
+    return 1;
 }
